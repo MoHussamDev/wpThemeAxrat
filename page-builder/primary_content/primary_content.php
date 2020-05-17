aa05d1362953e888681eae1c96da097992a97ff2
MoHussamDev <47823206+MoHussamDev@users.noreply.github.com>
2020-05-17 16:49:05 +0200

    added all modules to paddding

--- page-builder/primary_content/primary_content.php
+++ page-builder/primary_content/primary_content.php
@@ -6,10 +6,32 @@ if(!empty(get_sub_field('custom_class'))){
   $custom_class = '';
 }
 
+$margin_top ='xr-margin-top_';
+$margin_bottom = 'xr-margin-bottom_';
+$padding_top ='xr-padding-top_';
+$padding_bottom = 'xr-padding-bottom_';
+
+  
+if(have_rows('spacing')){
+    while(have_rows('spacing')){
+      the_row();
+      $margin_top =  $margin_top.get_sub_field('margin_top');
+      $margin_bottom = $margin_bottom.get_sub_field('margin_bottom');
+      $padding_top = $padding_top.get_sub_field('padding_top');
+      $padding_bottom = $padding_bottom.get_sub_field('padding_bottom');
+
+      };
+      
+  };
+ $spacing = implode(" ", array($margin_top,$margin_bottom, $padding_top,$padding_bottom ) );
+ 
+
+
+
 ;?>
 
 
-<section id="primaryContent" class="<?= $custom_class ?>">
+<section id="primaryContent" class="<?= $custom_class ?> <?= $spacing ?>">
  <div class="container">
     <div class="row ">
 
