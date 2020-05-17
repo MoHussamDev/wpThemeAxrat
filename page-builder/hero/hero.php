b5a9427b2311ed98150b3db2d8e3e8f569a381ba
MoHussamDev <47823206+MoHussamDev@users.noreply.github.com>
2020-05-17 14:19:12 +0200

    add/hero

--- page-builder/hero/hero.php
+++ page-builder/hero/hero.php
@@ -3,7 +3,15 @@
     $title = get_sub_field('title');
     $paragraph = get_sub_field('paragraph');
 ?>
+<section id="hero" style="
+    --background-image:url('<?= $image  ?>');">
+<div class="container">
+    <div class="hero-data">
 
-<h1>
-    hjkjhj
-</h1>
+        <h1><?=$title ?></h1>
+        <p><?=$paragraph ?></p>
+    </div>
+</div>
+
+
+</section>
